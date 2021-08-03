301bdc47be2edbadd8d7cfa3ab7eb52683caa6a8
Tom CHEN <jianfei.chen@gmail.com>
2021-08-03 15:13:40 -0700

    Added Nucleotide Count

---
+++ nucleotide-count/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+const NUCLEOTIDE: [char; 4] = ['A', 'T', 'C', 'G'];
+
+pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
+    nucleotide_counts(dna)?.get(&nucleotide).map(|c| *c).ok_or(nucleotide)
+}
+
+pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
+    Ok(dna.chars().fold(Ok(NUCLEOTIDE.iter().map(|c| (*c, 0)).collect::<HashMap<char, usize>>()),
+                        |hashmap_result, c| {
+                            hashmap_result.and_then(|mut hashmap| {
+                                if NUCLEOTIDE.contains(&c) {
+                                    *hashmap.entry(c).or_insert(0) += 1;
+                                    Ok(hashmap)
+                                } else {
+                                    Err(c)
+                                }
+                            })
+                        })?)
+}
