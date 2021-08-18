7ee12510c91df53217a48d80b336124cc6bf0cf2
Tom CHEN <jianfei.chen@gmail.com>
2021-08-18 12:32:12 -0700

    Robot Name

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,15 +1,44 @@
-pub struct Robot;
+use once_cell::sync::Lazy;
+use rand::random;
+use std::collections::HashSet;
+use std::sync::Mutex;
+
+static ROBOT_NAMES: Lazy<Mutex<HashSet<String>>> = Lazy::new(|| Mutex::new(HashSet::new()));
+
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        let mut r = Robot {
+            name: String::new(),
+        };
+        r.reset_name();
+        r
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.name
+    }
+
+    pub fn random_name() -> String {
+        format!(
+            "{}{}{:03}",
+            (random::<u8>() % 26 + b'A') as char,
+            (random::<u8>() % 26 + b'A') as char,
+            random::<u16>() % 1000
+        )
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        let mut name_set = ROBOT_NAMES.lock().unwrap();
+        let mut new_name = String::new();
+        while new_name.is_empty() || name_set.contains(&new_name) {
+            new_name = Self::random_name();
+        }
+        name_set.remove(&self.name as &str);
+        name_set.insert(new_name.clone());
+        self.name = new_name;
     }
 }
