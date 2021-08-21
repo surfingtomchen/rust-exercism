2ad1a5eaa239a806c1069e4e1bae52740d45405e
Tom CHEN <jianfei.chen@gmail.com>
2021-08-20 19:31:09 -0700

    Sublist - beautiful version

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,3 +1,6 @@
+use crate::Comparison::*;
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,37 +9,15 @@ pub enum Comparison {
     Unequal,
 }
 
-pub fn is_sub<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
-    if !first_list.is_empty() && second_list.is_empty() {
-        return false;
-    }
-
-    let first_item = first_list.iter().next();
-    let mut index = 0;
-    if second_list
-        .iter()
-        .skip_while(|item| {
-            index += 1;
-            Some(*item) != first_item
-        })
-        .zip(first_list.iter())
-        .filter(|(a, b)| a == b)
-        .count()
-        == first_list.len()
-    {
-        true
-    } else {
-        is_sub(first_list, &second_list[index..])
+pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
+    match first_list.len().cmp(&second_list.len()) {
+        Ordering::Less => if contains(second_list, first_list) { Sublist } else { Unequal }
+        Ordering::Equal => if first_list == second_list { Equal } else { Unequal },
+        Ordering::Greater => if contains(first_list, second_list) { Superlist } else { Unequal }
     }
 }
 
-pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
-    let is_sub_a_b = is_sub(first_list, second_list);
-    let is_sub_b_a = is_sub(second_list, first_list);
-    match (is_sub_a_b, is_sub_b_a) {
-        (true, true) => Comparison::Equal,
-        (true, false) => Comparison::Sublist,
-        (false, true) => Comparison::Superlist,
-        (false, false) => Comparison::Unequal,
-    }
+/// if a contains b
+pub fn contains<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    b.is_empty() || a.windows(b.len()).any(|list| list == b)
 }
