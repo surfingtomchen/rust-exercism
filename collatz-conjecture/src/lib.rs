c82a19255d21cc886b5169649160bb7caec4e52e
Tom CHEN <jianfei.chen@gmail.com>
2021-08-26 19:22:08 -0700

    Collatz Conjecture

---
+++ collatz-conjecture/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn collatz(n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+
+    let mut n = n;
+    let mut result = 0;
+    while n > 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        result += 1;
+    }
+    Some(result)
+}
