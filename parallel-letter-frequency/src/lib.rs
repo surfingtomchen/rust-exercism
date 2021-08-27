5b591994d443d54209215846ec9a34a291e18d09
Tom CHEN <jianfei.chen@gmail.com>
2021-08-27 16:24:34 -0700

    Parallel Letter Frequency - mpsc version

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,16 +1,15 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let result: Arc<Mutex<HashMap<char, usize>>> = Arc::new(Mutex::new(HashMap::new()));
+    let (tx, rx) = mpsc::channel();
+    let mut result: HashMap<char, usize> = HashMap::new();
     let mut handles = vec![];
 
     for lines in input.chunks(input.len() / worker_count + 1) {
-        let map = Arc::clone(&result);
-        let mut v = vec![];
-        lines.iter().for_each(|&line| v.push(line.to_owned()));
-
+        let v = lines.iter().map(|&x| x.to_owned()).collect::<Vec<_>>();
+        let tx = mpsc::Sender::clone(&tx);
         handles.push(thread::spawn(move || {
             let mut worker_result: HashMap<char, usize> = HashMap::new();
             v.iter().for_each(|line| {
@@ -18,17 +17,21 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
                     *worker_result.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
                 })
             });
-            let mut r = map.lock().unwrap();
-            worker_result
-                .iter()
-                .for_each(|(c, i)| *r.entry(*c).or_insert(0) += *i)
+            tx.send(worker_result).unwrap();
         }));
     }
 
     for h in handles {
-        h.join().unwrap()
+        h.join().unwrap();
     }
 
-    let hm = result.lock().unwrap().clone();
-    hm
+    drop(tx);
+
+    for received in rx {
+        received.iter().for_each(|(char, count)| {
+            *result.entry(*char).or_insert(0) += *count;
+        });
+    }
+
+    result
 }
