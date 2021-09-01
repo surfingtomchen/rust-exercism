4236d6b0a49f27afd301a344995d97d74a5cf4b9
Tom CHEN <jianfei.chen@gmail.com>
2021-09-01 15:09:06 -0700

    Parallel Letter Frequency - return thread result directly

--- parallel-letter-frequency/src/lib.rs
+++ parallel-letter-frequency/src/lib.rs
@@ -1,15 +1,12 @@
 use std::collections::HashMap;
-use std::sync::mpsc;
 use std::thread;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let (tx, rx) = mpsc::channel();
     let mut result: HashMap<char, usize> = HashMap::new();
     let mut handles = vec![];
 
     for lines in input.chunks(input.len() / worker_count + 1) {
         let v = lines.iter().map(|&x| x.to_owned()).collect::<Vec<_>>();
-        let tx = mpsc::Sender::clone(&tx);
         handles.push(thread::spawn(move || {
             let mut worker_result: HashMap<char, usize> = HashMap::new();
             v.iter().for_each(|line| {
@@ -17,18 +14,13 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
                     *worker_result.entry(c.to_ascii_lowercase()).or_insert(0) += 1;
                 })
             });
-            tx.send(worker_result).unwrap();
+            worker_result
         }));
     }
 
     for h in handles {
-        h.join().unwrap();
-    }
-
-    drop(tx);
-
-    for received in rx {
-        received.iter().for_each(|(char, count)| {
+        let worker_result = h.join().unwrap();
+        worker_result.iter().for_each(|(char, count)| {
             *result.entry(*char).or_insert(0) += *count;
         });
     }
