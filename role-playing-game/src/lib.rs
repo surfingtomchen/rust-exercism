21088f6c37c030b276e0b2ee4183c90411681460
Tom CHEN <jianfei.chen@gmail.com>
2021-09-01 17:16:38 -0700

    Role Playing Game

---
+++ role-playing-game/src/lib.rs
@@ -0,0 +1,35 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        match self.health {
+            0 => Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            }),
+            _ => None,
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        match self.mana {
+            Some(mana) => {
+                if mana > mana_cost {
+                    self.mana = Some(mana - mana_cost);
+                    mana_cost * 2
+                } else {
+                    0
+                }
+            }
+            _ => {
+                self.health -= mana_cost;
+                0
+            }
+        }
+    }
+}
