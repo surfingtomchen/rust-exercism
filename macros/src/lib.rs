f340f96c0e7b4d5d4876a8520ad5a2e7c5627f95
Tom CHEN <jianfei.chen@gmail.com>
2021-09-01 21:50:05 -0700

    Macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,19 @@
+#[macro_export]
+macro_rules! hashmap {
+
+    ( $($k:expr => $v:expr),*  ) => {
+        {
+            let mut temp_hashmap = ::std::collections::HashMap::new();
+            $(
+                temp_hashmap.insert( $k , $v );
+            )*
+            temp_hashmap
+        }
+    };
+
+    ( $($k:expr => $v:expr,)*  ) => {
+        {
+            macros::hashmap!( $( $k => $v),*)
+        }
+    };
+}
