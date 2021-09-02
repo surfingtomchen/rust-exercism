f405ad03157534156b2f6c88354c70bc6926389a
Tom CHEN <jianfei.chen@gmail.com>
2021-09-01 18:24:34 -0700

    Magazine Cutout

---
+++ magazine-cutout/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
+    let magazine_words = magazine.iter().fold(HashMap::new(), |mut words, str| {
+        *words.entry(str).or_insert(0) += 1;
+        words
+    });
+    let note_words = note.iter().fold(HashMap::new(), |mut words, str| {
+        *words.entry(str).or_insert(0) += 1;
+        words
+    });
+
+    note_words
+        .iter()
+        .all(&|(w, count)| magazine_words.get(w).unwrap_or(&0) >= count)
+}
